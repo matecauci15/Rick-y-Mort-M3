f8447779b0c503120ab8b7175b7ed7ce08294b65
matecauci15 <matecaucino@gmail.com>
2023-05-01 12:18:36 -0300

    m3final

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -6,5 +6,12 @@ const About = () => {
         </div>
     )
 }
-
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>React in progress</h1>
+//             <p>Estamos laburando</p>
+//         </div>
+//     )
+// }
 export default About;
